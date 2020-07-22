9145fc06758366cfdfa86b8aeff0f14e57f27e83
gokulnathsj <gokulnathsj@gmail.com>
2020-07-22 17:45:17 +0530

    files add

--- Petrichor CA Portal/Petrichor CA Portal GNSJ/models/user.js
+++ Petrichor CA Portal/Petrichor CA Portal GNSJ/models/user.js
@@ -2,11 +2,15 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
+    points: Number,
+    token_id: String,
     name: String,
     email: String,
     username: String,
     password: String,
     college: String,
+    registrations: Number,
+    rank: String,
 });
 
 UserSchema.plugin(passportLocalMongoose);
