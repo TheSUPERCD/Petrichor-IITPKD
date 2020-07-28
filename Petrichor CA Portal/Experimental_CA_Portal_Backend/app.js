4044488e093623ee93e7ad26e698833117684fc7
TheSUPERCD <supercdeb2000@gmail.com>
2020-07-28 22:20:45 +0530

    Integrated Gokul's work and Experimental folder

--- Petrichor CA Portal/Experimental_CA_Portal_Backend/app.js
+++ Petrichor CA Portal/Experimental_CA_Portal_Backend/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require("path");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -9,8 +10,12 @@ var User = require('./models/user')
 var nodemailer = require('nodemailer');
 var CryptoJS = require('crypto-js');
 const user = require('./models/user');
+var flush = require("connect-flash");
 var crypto_key = 'secureKey';
 
+var initial_reg = 0;
+initial_rank = "NIL";
+initial_points = 0;
 
 var smtpTransport = nodemailer.createTransport({
     service: "gmail",
@@ -36,9 +41,13 @@ app.use(express.static('public'));
 passport.use(new localStrategy(User.authenticate()));
 app.use(require('express-session')({
     secret: 'secret',
+    cookie: { expires : false},
     resave: false,
     saveUninitialized: false
 }));
+
+app.use(flush())
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -48,19 +57,45 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
-app.get('/', isLoggedIn, function(req, res){
-    console.log('/ got a request sir.');
-    res.render('Profile_template.ejs');
+app.get("/dashboard", isLoggedIn, function(req, res) {
+    var details;
+    mongoose.connect(
+        'mongodb+srv://adminuser:adminpass@cluster0-c469f.mongodb.net/CA_Portal_Users?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        function(err, db) {
+            if (err) {
+                res.redirect("/login");
+            } else {
+                db.collection("users")
+                    .find({ username: req.user.username })
+                    .toArray(function(err, docs) {
+                        details = docs;
+                        res.render("Dashboard.ejs", { user_info: details });
+                    });
+            }
+        }
+    );
 });
 
-app.get('/login', function(req, res){
+
+app.get('/login', isLoggedOut, function(req, res){
     console.log('/login got a request sir.');
-    res.render('Login_form.ejs');
+    res.render("login.ejs", { string: req.flash('error') });
 });
 
-app.get('/signup', function(req, res){
+app.get('/signup', isLoggedOut, function(req, res){
     console.log('/signup got a request sir.');
-    res.render('signup.ejs');
+    res.render('signup.ejs', { string: req.flash('alert') });
+});
+
+app.get("/profile", isLoggedIn, function(req, res) {
+    res.render("profile.ejs");
+});
+
+app.get("/registrations", isLoggedIn, function(req, res) {
+    res.render("Registrations.ejs");
 });
 
 app.get('/logout', isLoggedIn, function(req, res){
@@ -68,9 +103,9 @@ app.get('/logout', isLoggedIn, function(req, res){
     res.send('You have logged out successfully');
 });
 
-app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), function(req, res){
+app.post('/login', passport.authenticate('local', {failureRedirect: '/login', failureFlash: 'Invalid Username or Passsword'}), function(req, res){
     if(req.user.isVerified){
-        res.redirect('/');
+        res.redirect('/dashboard');
     }
     else{
         req.logOut();
@@ -90,11 +125,22 @@ app.post('/signup', function(req, res){
         }
         else{
             if(user.length !== 0){
+                req.flash('alert', 'User Already Exists');
                 res.redirect('/signup'); // username or email already taken
             }
             else{
                 // Registering user
-                User.register(new User({name:req.body.name, username:req.body.username, email:req.body.email, isVerified:false, college:req.body.clg}),
+                User.register(new User({
+                    name:req.body.name,
+                    username:req.body.username,
+                    email:req.body.email,
+                    isVerified:false,
+                    college:req.body.clg,
+                    points: initial_points,
+                    registrations: initial_reg,
+                    rank: initial_rank,
+                    token_id: get_token(8),
+                }),
                 req.body.password, function(err, newuser){
                     if(err){
                         console.log('error in creating a new user');
@@ -110,6 +156,7 @@ app.post('/signup', function(req, res){
                 host=req.get('host');
                 link="http://"+req.get('host')+"/verify?id="+rand;
                 mailOptions={
+                    from : 'sandywill6969@gmail.com',
                     to : req.body.email,
                     subject : "Please confirm your Email account",
                     html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
@@ -170,13 +217,54 @@ app.get('/verify',function(req,res){
     }
 });
 
+app.get("/admin/registrations", function(req, res) {
+    res.render("rank_update.ejs", { message: req.flash('message') });
+});
+
+app.get("/ca-information", function(req, res) {
+    var detail_info = [];
+    mongoose.connect(
+        'mongodb+srv://adminuser:adminpass@cluster0-c469f.mongodb.net/CA_Portal_Users?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        function(err, db) {
+            var details = db.collection("users").find();
+            details.forEach(
+                function(data, err) {
+                    detail_info.push(data);
+                },
+                function() {
+                    res.render("ca-details.ejs", { details: detail_info });
+                }
+            );
+        }
+    );
+});
+
+app.post("/admin/registrations", function(req, res) {
+    var token = req.body.token;
+    var num_reg = parseInt(req.body.registrations);
+    var points = parseInt(req.body.points);
+    User.findOneAndUpdate({ token_id: token }, { $inc: { registrations: num_reg, points: points } }, function(error, result) {
+        if (error) {
+            req.flash('message', 'Submission Failed');
+            res.redirect("/admin/registrations");
+        } else if (!result) {
+            req.flash('message', 'Incorrect Details, No user Exists');
+            res.redirect("/admin/registrations");
+        } else {
+            req.flash('message', 'Saved successfully')
+            res.redirect("/admin/registrations")
+        }
+    });
+});
 
 
 
 
 
-
-app.get('*', function(req, res){
+app.get('/*', function(req, res){
     res.send('ERROR 404 :The page you requested does not exist !');
 });
 
@@ -190,6 +278,39 @@ function isLoggedIn(req, res, next){
     }
 }
 
+function isLoggedOut(req, res, next){
+    if(req.isAuthenticated()){
+        res.redirect('/dashboard');
+    }
+    else{
+        return next();
+    }
+}
+
+function get_token(length) {
+    var result = "";
+    var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    User.find({token_id : result}, function (err, user){
+        if(err){
+            console.log('some error occurred');
+        }
+        else{
+            if(user.length === 0){
+                console.log('No users with the token id found');
+            }
+            else{
+                console.log('Users with the token id found');
+                get_token(length);
+            }
+        }
+    });
+    return result;
+}
 
 app.listen(8000, function(){
     console.log('Starting Server.....');
--- Petrichor CA Portal/Experimental_CA_Portal_Backend/models/user.js
+++ Petrichor CA Portal/Experimental_CA_Portal_Backend/models/user.js
@@ -8,7 +8,11 @@ var userSchema = new mongoose.Schema({
     email: String,
     college: String,
     isVerified: Boolean,
-    password: String
+    password: String,
+    points: Number,
+    registrations: Number,
+    rank: String,
+    token_id: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
