9c24ed85179bf0048f5f41bfa7023d979fa183cd
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-07-30 21:10:55 +0530

    added two things(update password and email checking)

--- Petrichor CA Portal/CA_Portal_Backend_Vinay/app.js
+++ Petrichor CA Portal/CA_Portal_Backend_Vinay/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require("path");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -9,7 +10,7 @@ var User = require('./models/user')
 var nodemailer = require('nodemailer');
 var CryptoJS = require('crypto-js');
 const user = require('./models/user');
-const SMTPTransport = require('nodemailer/lib/smtp-transport');
+const { db } = require('./models/user');
 var crypto_key = 'secureKey';
 
 var transport = nodemailer.createTransport({
@@ -67,7 +68,7 @@ app.get('/signup', function(req, res){
     res.render('signup.ejs');
 });
 
-app.get('/forgot', function(req, res){
+app.get('/forgot',function(req, res){
     console.log('/forgot password got a request sir.');
     res.render('forgot_Password.ejs');
 });
@@ -77,7 +78,7 @@ app.get('/changePassword', function(req, res){
     res.render('change_Password.ejs');
 });
 
-app.get('/logout', isLoggedIn, function(req, res){
+app.get('/logout', function(req, res){
     req.logOut();
     res.send('You have logged out successfully');
 });
@@ -121,11 +122,12 @@ app.post('/signup', function(req, res){
                 });
 
 
-                // Sending verification email
+                //Sending verification email
                 // rand=CryptoJS.AES.encrypt(req.body.email, crypto_key);
                 // host=req.get('host');
                 // link="http://"+req.get('host')+"/verify?id="+rand;
                 // mailOptions={
+                //     from: 'pulavartivinay@gmail.com',
                 //     to : req.body.email,
                 //     subject : "Please confirm your Email account",
                 //     html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
@@ -151,9 +153,9 @@ app.post('/signup', function(req, res){
 var forgot_email;
 app.post('/forgot', function(req, res){
         forgot_email = req.body.email;
-        rand=CryptoJS.AES.encrypt(req.body.email, crypto_key);
+        rand=CryptoJS.AES.encrypt(req.body.email, crypto_key).toString();
         host=req.get('host');
-        link="http://"+req.get('host')+"/changePassword";
+        link="http://"+req.get('host')+"/intermediate?id="+rand;
         console.log(link);
         User.find({email: req.body.email},function(err,user){
             if(err){
@@ -174,7 +176,7 @@ app.post('/forgot', function(req, res){
             to: req.body.email,
             subject: 'Email verification step to proceed for reset password',
             text: `Please click the below link to reset ur password`,
-            html: '<h1>Click this link</h1><a href = "http://localhost:27017/changePassword">Reset Password</a>'
+            html: '<h1>Click this link</h1><a href = '+link+'>Reset Password</a>'
         };
         sendmailcorrectly = function(){
             transport.sendMail(mailOptions,function(error,response){
@@ -195,11 +197,28 @@ app.post('/changePassword', function(req, res){
     console.log(forgot_pass);
     User.findOne({email: forgot_email}, function(err, user){
         if(err)return handleErr(err);
-        user.password = forgot_pass;
-        user.save(function(err){
-           if(err)return handleErr(err);
-           //user has been updated
+        var temp_name = user.name;
+        var temp_username = user.username;
+        var temp_email = user.email;
+        var temp_college = user.college;
+        User.findByIdAndRemove({_id: user.id}, 
+            function(err, docs){
+             if(err) console.log('error occured in user removing');
+             else    console.log('user removed');;
          });
+        User.register(new User({name:temp_name, username:temp_username, email:temp_email, college:temp_college}),
+                forgot_pass, function(err, newuser){
+                    if(err){
+                        console.log('error in creating a new user');
+                    }
+                    else{
+                        console.log(newuser);
+                    }
+                });
+        // user.save(function(err){
+        //    if(err)return handleErr(err);
+        //    //user has been updated
+        //  });
        });
     console.log('Password changed succesfully');
     // res.send("Password changed successfully !!!")
@@ -236,6 +255,21 @@ app.post('/profile_data_post', function(req, res){
 //         res.send("<h1>Request is from unknown source");
 //     }
 // });
+
+app.get('/intermediate',function(req,res){
+    if((req.protocol+"://"+req.get('host'))==("http://"+host)){
+        console.log("Domain is matched. Information is from Authentic email");
+        console.log(req.query.id.replace(/\s/g, '+'));
+        var toVerifyEmail = CryptoJS.AES.decrypt(req.query.id.replace(/\s/g, '+'), crypto_key).toString(CryptoJS.enc.Utf8);
+        console.log(toVerifyEmail);
+        if(forgot_email == toVerifyEmail){
+            res.redirect('/changePassword');
+        }
+        else{
+            res.redirect('/signup');
+        }     
+    }
+});
 
 
 
